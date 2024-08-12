c3454cfaed1ec3b53c71dea9b9f73219c2025323
OELS <oelsart@gmail.com>
2024-08-12 13:53:50 +0900

    斜めに通れる壁のdefNameリストをxmlに公開

---
+++ Source/PassingBetweenDiagonalWalls/PassingBetweenDiagonalWalls.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Verse;
+
+namespace PassingBetweenDiagonalWalls
+{
+    [StaticConstructorOnStartup]
+    public class PassingBetweenDiagonalWalls : Mod
+    {
+        public PassingBetweenDiagonalWalls(ModContentPack content) : base(content)
+        {
+            PassingBetweenDiagonalWalls.content = content;
+            PassingBetweenDiagonalWalls.ExposeData();
+        }
+
+        public static ModContentPack content;
+
+        public static List<string> diagonalWallDefNames;
+
+        public static void ExposeData()
+        {
+            using (StreamReader streamReader = new StreamReader(content.RootDir + "/DiagonalWallList.xml"))
+            {
+                using (XmlTextReader xmlTextReader = new XmlTextReader(streamReader))
+                {
+                    XmlDocument xmlDocument = new XmlDocument();
+                    xmlDocument.Load(xmlTextReader);
+                    var curXmlParent = xmlDocument.DocumentElement;
+
+                    PassingBetweenDiagonalWalls.diagonalWallDefNames = new List<string>(curXmlParent.ChildNodes.Count);
+                    foreach (var node in curXmlParent.ChildNodes)
+                    {
+                        var name = ScribeExtractor.ValueFromNode<string>((XmlNode)node, default(string));
+                        PassingBetweenDiagonalWalls.diagonalWallDefNames.Add(name);
+                    }
+                }
+            }
+        }
+    }
+}
